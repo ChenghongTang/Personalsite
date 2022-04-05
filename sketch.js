965503c9e66b2a4be6bd46b552dc4ab4f7b8b59a
ChenghongTang <ctang12@pratt.edu>
2022-04-05 10:42:53 -0400

    Update sketch.js
    d

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 
-let theImage;
-function preload(){
-  theImage = loadImage("photo.png")
-}
+// let theImage;
+// function preload(){
+//   theImage = loadImage("photo.png")
+// }
 
 
-function setup(){
-    createCanvas(windowWidth,windowHeight)
-    background(100)
-}
+// function setup(){
+//     createCanvas(windowWidth,windowHeight)
+//     background(100)
+// }
 
 
 
-function draw() {
-    noStroke()
-    fill(255,0,0);
-    ellipse(mouseX,mouseY,100,100)
-}
+// function draw() {
+//     noStroke()
+//     fill(255,0,0);
+//     ellipse(mouseX,mouseY,100,100)
+// }
